b18bb884a212163a9dccc3e64b0f335b06d000bf
Arun <arungarg57460@gmail.com>
2026-02-20 17:54:24 +0530

    Added get route for classroom to fetch all classroom current users is currently joined in / created

--- backend/routes/classroom.js
+++ backend/routes/classroom.js
@@ -4,6 +4,29 @@ import { protect,teacherOnly } from '../middleware/auth.js';
 
 const router = express.Router();
 
+router.get('/get',protect,async(req,res)=>{
+    try{
+        let classroom;
+        if(req.user.role==='teacher'){
+            classroom=await Classroom.find({ teacher:req.user.id}).populate('students','username email').sort({createdAt: -1});
+        }
+        else{
+            classroom=await Classroom.find({ students: req.user.id }).populate('teacher','username email').sort({createdAt: -1});
+        }
+        res.json({
+            success: true,
+            role: req.user.role,
+            classroom
+        });
+    }catch(error){
+        console.log('Error fetching classrooms: ',error);
+        res.status(500).json({
+            success:false,
+            msg:"Server error"
+        });
+    }
+})
+
 router.post('/create',protect,teacherOnly,async (req,res)=>{
     try{
         const { name } = req.body;
@@ -46,7 +69,7 @@ router.post('/join',protect,async(req,res)=>{
         classroom.students.push(req.user.id);
         await classroom.save();
 
-        res.json({msg : "Joined Classroom Successfully!!",classRoomId:classroom._id});
+        res.json({msg : "Joined Classroom Successfully!!",classroom:{id:classroom._id,name:classroom.name,joinCode:classroom.joinCode}});
     }catch(err){
         console.error(err);
         res.status(500).json({ msg: 'Server error' });

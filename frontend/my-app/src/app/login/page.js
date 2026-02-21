1cf54557b81ff903fd0b1228da7376fba241641c
Arun <arungarg57460@gmail.com>
2026-02-21 22:38:57 +0530

    Added UI to Login but a little work left for better design

--- frontend/my-app/src/app/login/page.js
+++ frontend/my-app/src/app/login/page.js
@@ -1,5 +1,6 @@
 'use client'
-import { useState } from "react";
+import Link from "next/link";
+import { useState,useEffect, use } from "react";
 
 // Example Teacher Login for testing
 //   "email": "arun@teacher.com",
@@ -8,12 +9,19 @@ import { useState } from "react";
 //   "email": "student@example.com",
 //   "password": "student123",
 export default function Login() {
-    if(localStorage.getItem('token')){
-        setTimeout(()=>window.location.href="/",1000);
-    }
-
+    
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [token, setToken] = useState(null);
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem("token");
+
+        if (storedToken) {
+            setToken(storedToken);
+            window.location.href = "/";
+        }
+    }, []);
     // console.log("Current Email ",email);
 
     const handleLogin = async (e) => {
@@ -46,13 +54,25 @@ export default function Login() {
 
 
     return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleLogin}>
-                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit">Login</button>
+    
+    <div className="min-h-screen bg-gray-100 flex item-center justify-center">
+        <div className="bg-white p-8 rouded-lg shadow-lg w-full max-w-md">
+            <h1 className="text-3xl font-bold text-center mb-6 text-gray-800 ">Login</h1>
+            <form onSubmit={handleLogin} className="">
+                <div>
+                <label htmlFor="emailInput" className="block text-sm font-medium text-gray-700 mb-1">Email</label>
+                <input type="email" id="emailInput" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500" />
+                </div>
+                <div>
+                <label htmlFor="passWord" className="block text-sm font-medium text-gray-700 mb-1">Password</label>
+                <input type="password" id="passWord" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-500 focus:border-indigo"/>
+                </div>
+                <button type="submit" className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transtion-colors mt-2">Login</button>
             </form>
+            <p className="mt-4 text-center text-sm text-gray-600">
+                Don't have an Account ?<Link href="/register" className="text-indigo-600 hover:underline"> Register here</Link> 
+            </p>
         </div>
+    </div>
     )
 }
